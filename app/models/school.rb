bd26bb645c78c5a453ec9a04b2f15ab235bdab1e
JustinShi001 <neoboy0612@gmail.com>
2019-12-06 16:28:51 -0500

    Edited school model for input validation requirements

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,3 +1,9 @@
 class School < ApplicationRecord
 has_many :houses
+
+# validates :name, uniqueness: true, inclusion: { in: 'School',
+#   message: "Name must include the phrase School"
+# }
+# validates :location, presence: true
+# validates :owner, presence: true
 end
